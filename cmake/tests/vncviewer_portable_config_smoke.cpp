7eaf05a1a5cb7032c3e16bb13a97afa91326e07f
Marco Fortina <marco_fortina@hotmail.it>
2026-05-18 07:44:16 +0000

    cmake: test native Linux portable viewer config

---
+++ cmake/tests/vncviewer_portable_config_smoke.cpp
@@ -0,0 +1,45 @@
+// This file is part of UltraVNC
+// https://github.com/ultravnc/UltraVNC
+// https://uvnc.com/
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// SPDX-FileCopyrightText: Copyright (C) 2002-2025 UltraVNC Team Members. All Rights Reserved.
+
+#include "vncPortableViewerConfig.h"
+
+#include <cassert>
+#include <string>
+
+using uvnc::vncviewer::portable::ViewerConfig;
+
+int main()
+{
+    ViewerConfig config;
+    std::string error;
+
+    assert(config.Host() == "127.0.0.1");
+    assert(config.Port() == 5900);
+    assert(config.Shared());
+    assert(!config.RequestUpdate());
+    assert(!config.ViewOnly());
+    assert(config.Validate(&error));
+    assert(error.empty());
+
+    config.SetHost("");
+    assert(!config.Validate(&error));
+    assert(error == "viewer host must not be empty");
+
+    config.SetHost("127.0.0.1");
+    config.SetPort(0);
+    assert(!config.Validate(&error));
+    assert(error == "viewer port must not be zero");
+
+    config.SetPort(5901);
+    config.SetRequestUpdate(true);
+    config.SetViewOnly(true);
+    assert(config.Validate(&error));
+    assert(config.RequestUpdate());
+    assert(config.ViewOnly());
+    return 0;
+}
