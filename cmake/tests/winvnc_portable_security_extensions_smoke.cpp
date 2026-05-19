c4687728fe02c27a2d714800b8eb9f35ced7d1a9
Marco Fortina <marco_fortina@hotmail.it>
2026-05-19 17:55:03 +0000

    cmake: cover exact legacy security extension catalog

--- cmake/tests/winvnc_portable_security_extensions_smoke.cpp
+++ cmake/tests/winvnc_portable_security_extensions_smoke.cpp
@@ -18,13 +18,23 @@ int main()
     SecurityExtensionDecision dsm = EvaluateSecurityExtension(ParseSecurityExtensionOption("--dsm-plugin"));
     assert(dsm.kind == SecurityExtensionKind::DsmPlugin);
     assert(!dsm.supported);
-    assert(dsm.reason.find("Windows DLL") != std::string::npos);
-    assert(dsm.replacement.find("vencrypt") != std::string::npos || dsm.replacement.find("VeNCrypt") != std::string::npos);
+    assert(dsm.reason.find("DSMPlugin DLL ABI") != std::string::npos);
+    assert(dsm.originalImplementation.find("DSMPlugin") != std::string::npos);
+
+    SecurityExtensionDecision securevnc = EvaluateSecurityExtension(ParseSecurityExtensionOption("--securevnc-plugin"));
+    assert(securevnc.kind == SecurityExtensionKind::SecureVncPlugin);
+    assert(!securevnc.supported);
+    assert(securevnc.wireType == rfbUltraVNC_SecureVNCPluginAuth_new);
 
     SecurityExtensionDecision mslogon = EvaluateSecurityExtension(ParseSecurityExtensionOption("--mslogon"));
-    assert(mslogon.kind == SecurityExtensionKind::MsLogon);
+    assert(mslogon.kind == SecurityExtensionKind::MsLogonII);
     assert(!mslogon.supported);
-    assert(mslogon.replacement.find("PAM") != std::string::npos);
+    assert(mslogon.wireType == rfbUltraVNC_MsLogonIIAuth);
+    assert(mslogon.replacement.find("viewer can speak MSLogonII") != std::string::npos);
+
+    SecurityExtensionDecision mslogonI = EvaluateSecurityExtension(ParseSecurityExtensionOption("--mslogon-i"));
+    assert(mslogonI.kind == SecurityExtensionKind::MsLogonI);
+    assert(mslogonI.wireType == rfbUltraVNC_MsLogonIAuth);
 
     SecurityExtensionDecision javaViewer = EvaluateSecurityExtension(ParseSecurityExtensionOption("--http-java-viewer"));
     assert(javaViewer.kind == SecurityExtensionKind::HttpJavaViewer);
